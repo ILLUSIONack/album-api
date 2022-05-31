aaa2cbcf972a68e80455666b500a1226d6ac0307
Usman <usman-ack@hotmail.com>
2022-05-31 14:55:38 +0200

    Push album.api

---
+++ Album-Api/Album.Api/Controllers/HelloController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Album.Api.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Album.Api.Controllers
+{
+    public class HelloController : Controller
+    {
+        [HttpGet("/api/hello")]
+        public IActionResult Index(string? name = null)
+        {
+            var greetingService = new GreetingService();
+            var result = greetingService.greet(name);
+            return Ok(result);
+        }
+    }
+}
+
